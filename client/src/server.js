18996bdb7aa0741ca498018f09b277f6bc64f6a1
Dharaneesh Boobalan <dharaneesh0745@gmail.com>
2024-08-26 10:37:54 +0530

    Update server.js

--- client/src/server.js
+++ client/src/server.js
@@ -1,5 +1,5 @@
-// export const server = "https://sih-hackathon.onrender.com/api/v1";
-export const server = "http://localhost:8000/api/v1";
+export const server = "https://sih-hackathon.onrender.com/api/v1";
+// export const server = "http://localhost:8000/api/v1";
 
-// export const backend_API_endpoint = "https://sih-hackathon.onrender.com/";
-export const backend_API_endpoint = "http://localhost:8000/";
+export const backend_API_endpoint = "https://sih-hackathon.onrender.com/";
+// export const backend_API_endpoint = "http://localhost:8000/";
