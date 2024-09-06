a34125eef49253f7bdb933f6b536327609fa3fff
Dharaneesh0745 <dharaneesh0745@gmail.com>
2024-09-06 09:26:10 +0530

    Seperate API endpoint for the bots.

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,9 @@
-// export const server = "https://sih-hackathon.onrender.com/api/v1";
-export const server = "http://localhost:8000/api/v1";
+export const server = "https://sih-hackathon.onrender.com/api/v1";
+// export const server = "http://localhost:8000/api/v1";
 
-// export const backend_API_endpoint = "https://sih-hackathon.onrender.com/";
-export const backend_API_endpoint = "http://localhost:8000/";
+export const backend_API_endpoint = "https://sih-hackathon.onrender.com/";
+// export const backend_API_endpoint = "http://localhost:8000/";
 
-export const bot_API_endpoint = "http://localhost:7000/api/v1";
+export const bot_API_endpoint =
+  "https://sih-hackathon-bots.onrender.com/api/v1";
+// export const bot_API_endpoint = "http://localhost:7000/api/v1";
